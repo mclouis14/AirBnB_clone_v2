cc6ab53357193a0d2bf4542b68ab5a7a4f31c55c
root <louis.officialz@gmail.com>
2024-04-22 13:41:55 +0000

    updated web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Starts a Flask web application
-
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
@@ -22,3 +21,5 @@ def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
